a6e0c94d19c23f0b0ee39cb35bfe49c8c5bf9ee1
Marina Marques <marina.marqueso@outlook.com>
2021-12-16 18:11:19 -0300

    Implementa requisiçoes API Mercado Livre, e adiciona componente Categoria.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 import React, { useContext } from "react";
-import globalContext from "../context/globalContext";
 
 function Header() {
-  const { state } = useContext(globalContext)
   return (
-    <header>createStore({ state })</header>
+    <header>createStore()</header>
   )
 }
 

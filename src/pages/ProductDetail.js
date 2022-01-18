9c55ff72b6493934ee3e57f06ef0f08d15ce9780
Marina Marques <marina.marqueso@outlook.com>
2022-01-17 21:14:26 -0300

    Começa estilizaçao da pagina de detalhes

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import globalContext from '../context/globalContext';
+import '../style/ProductDetail.css';
 
 export default function ProductDetail() {
   const navigate = useNavigate()
   const { details, addToCart } = useContext(globalContext);
   return(
-    <div>
-      <section>
+    <div
+      className='product-detail'
+    >
+      <section
+        className='product-to-detail'
+      >
         <h3>
           { details.title }
         </h3>
@@ -16,28 +21,32 @@ export default function ProductDetail() {
           alt={ details.title }
           width="200px"
         />
-        <section>
-          <table>
-            <tr>
-              <th>Cidade</th>
-              <th>Estado</th>
-            </tr>
-            <tr>
-              <td>{ details.address.city_name }</td>
-              <td>{ details.address.state_name }</td>
-            </tr>
-          </table>        
-        </section>
-        <section>
-          <h4>Especificações técnicas</h4>
-          <ul>
-            {
-              details.attributes.map((att) => (
-                <li>{ att.name }: { att.value_name }</li>
-              ))
-            }
-          </ul>
-        </section>
+      </section>
+      <section
+        className='product-localization'
+      >
+        <table>
+          <tr>
+            <th>Cidade</th>
+            <th>Estado</th>
+          </tr>
+          <tr>
+            <td>{ details.address.city_name }</td>
+            <td>{ details.address.state_name }</td>
+          </tr>
+        </table>        
+      </section>
+      <section
+        className='product-specification'
+      >
+        <h4>Especificações técnicas</h4>
+        <ul>
+          {
+            details.attributes.map((att) => (
+              <li>{ att.name }: { att.value_name }</li>
+            ))
+          }
+        </ul>
         <table>
           <tr>
             <th>Estoque</th>
@@ -47,11 +56,11 @@ export default function ProductDetail() {
             <td>{ details.available_quantity }</td>
             <td>{ details.sold_quantity }</td>
           </tr>
-          </table>
-        <button
-          onClick={() => addToCart(details)}
-        >Adicionar ao Carrinho</button>
+        </table>
       </section>
+      <button
+        onClick={() => addToCart(details)}
+      >Adicionar ao Carrinho</button>
       <button
         onClick={() => navigate('/')}
       >Voltar</button>
